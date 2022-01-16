dd94b24c9fc000eac83f325325afe8fb14a13fcb
RoseQSun <qsun75@gatech.edu>
2022-01-16 17:41:53 -0500

    Completed Exercise 1: audio file read

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <fstream>
 
 #include "MUSI6106Config.h"
 
@@ -33,25 +34,56 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
- 
+    if (argc < 3)
+        return -1;
+    sInputFilePath = argv[1];
+    sOutputFilePath = argv[2];
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
- 
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead, &stFileSpec);
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
- 
+    hOutputFile.open(sOutputFilePath);
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
- 
-    //////////////////////////////////////////////////////////////////////////////
+    long long iLength = 0;
+    phAudioFile->getLength(iLength);
+    auto iNumChannels = stFileSpec.iNumChannels;
+
+
+    ppfAudioData = new float* [iNumChannels];
+    for (int i=0; i<iNumChannels; ++i)
+        ppfAudioData[i] = new float[kBlockSize];
+
+    ////////////////////////////////////////iLength//////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
-
+    for (long long pos=0; pos<iLength; pos+=kBlockSize) {
+        long long iReadLength = 0;
+        if (iLength - pos <= kBlockSize)
+            iReadLength = kBlockSize;
+        else
+            iReadLength = iLength - pos;
+        phAudioFile->readData(ppfAudioData, iReadLength);
+        for (long long i=0; i<iReadLength; ++i) {
+            for (int c=0; c<iNumChannels; ++c) {
+                hOutputFile << ppfAudioData[c][i];
+                if (c < iNumChannels - 1)
+                    hOutputFile << ", ";
+                else
+                    hOutputFile << "\n";
+            }
+        }
+    }
     //////////////////////////////////////////////////////////////////////////////
-    // clean-up (close files and free memory)
-
+    // clean-up (close files and free memory
+    for (int i=0; i<iNumChannels; ++i)
+        delete[] ppfAudioData[i];
+    delete[] ppfAudioData;
+    ppfAudioData = nullptr;
+    phAudioFile->closeFile();
+    hOutputFile.close();
     // all done
     return 0;
-
 }
 
 
