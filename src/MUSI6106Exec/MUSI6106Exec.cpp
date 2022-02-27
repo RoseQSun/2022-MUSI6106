889db6fbfb47f415041760ee46e304be34c531e9
RoseQSun <qsun75@gatech.edu>
2022-02-27 02:49:40 -0500

    Update MUSI6106Exec.cpp

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -6,7 +6,6 @@
 
 #include "AudioFileIf.h"
 #include "Fft.h"
-#include "RingBuffer.h"
 
 using std::cout;
 using std::endl;
@@ -18,25 +17,26 @@ void    showClInfo();
 // main function
 int main(int argc, char* argv[])
 {
-    std::string             sInputFilePath, sOutputFilePath;
+    std::string             sInputFilePath,sOutputFilePath;
 
     static const int        kBlockSize = 1024;
+    int iBlockSize = 4096;
+    int iHopSize = 2048;
 
     clock_t                 time = 0;
 
-    float** ppfAudioData = 0;
-    float* pfMag = 0; //magnitude
+    float **ppfAudioData = 0;
+    CFft *pFft = 0;
+    float **ppfBlock = 0;
+    CFft::complex_t **ppFftOut = 0;
+    float **ppfMagnitude = 0;
 
-    CRingBuffer<float>* pRingBuffer = 0;
-    CFft* pFft = 0;
-
+    float **ppfBuffer = 0;
 
     CAudioFileIf* phAudioFile = 0;
     std::fstream            hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
 
-    int iBlockLength = 0;
-    int iHopLength = 0;
 
     showClInfo();
 
@@ -51,6 +51,13 @@ int main(int argc, char* argv[])
     {
         sInputFilePath = argv[1];
         sOutputFilePath = sInputFilePath + ".txt";
+        if (argc == 3) {
+            iBlockSize = atoi(argv[2]);
+        }
+        if (argc == 4) {
+            iBlockSize = atoi(argv[2]);
+            iHopSize = atoi(argv[3]);
+        }
     }
 
     //////////////////////////////////////////////////////////////////////////////
@@ -64,6 +71,7 @@ int main(int argc, char* argv[])
         return -1;
     }
     phAudioFile->getFileSpec(stFileSpec);
+    assert(stFileSpec.iNumChannels == 1);
 
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
@@ -78,8 +86,12 @@ int main(int argc, char* argv[])
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
     ppfAudioData = new float* [stFileSpec.iNumChannels];
-    for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        ppfAudioData[i] = new float[kBlockSize];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++) {
+        ppfAudioData[i] = new float[iBlockSize];
+        for (int j = 0; j < iBlockSize; j++){
+            ppfAudioData[i][j] = 0;
+        }
+    }
 
     if (ppfAudioData == 0)
     {
@@ -94,36 +106,64 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    ppfBuffer = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++) {
+        ppfBuffer[i] = new float[iBlockSize];
+        for (int j = 0; j < iBlockSize; j++){
+            ppfBuffer[i][j] = 0;
+        }
+    }
+
+    ppfBlock = new float*[1];
+    ppfBlock[0] = new float[iBlockSize];
+
+    ppFftOut = new float*[1];
+    ppFftOut[0] = new CFft::complex_t[iBlockSize];
+
+    ppfMagnitude = new float*[1];
+    ppfMagnitude[0] = new float[iBlockSize];
+
+
     time = clock();
+
     //////////////////////////////////////////////////////////////////////////////
     // initializing
-    pRingBuffer = new CRingBuffer<float>(kBlockSize);
-
     CFft::createInstance(pFft);
-    pFft->initInstance(kBlockSize);
-
+    pFft->initInstance(iBlockSize,1,CFft::kWindowHann,CFft::kPreWindow);
 
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
     while (!phAudioFile->isEof())
     {
         // set block length variable
-        long long iNumFrames = kBlockSize;
+        long long iNumFrames = iHopSize;
+        int iReadIdx = 0;
+        int iWriteIdx = iBlockSize - iHopSize;
 
         // read data (iNumOfFrames might be updated!)
         phAudioFile->readData(ppfAudioData, iNumFrames);
 
+        for (int i = 0; i < iNumFrames; i++){
+            ppfBuffer[0][iWriteIdx] = ppfAudioData[0][i];
+            iWriteIdx = (iWriteIdx + 1) % iBlockSize;
+        }
+        for (int i = 0; i < iBlockSize - iHopSize + iNumFrames; i++) {
+            ppfBlock[0][i] = ppfBuffer[0][iReadIdx];
+            iReadIdx = (iReadIdx + 1) % iBlockSize;
+        }
+        pFft->doFft(ppFftOut[0],ppfBlock[0]);
+        pFft->getMagnitude(ppfMagnitude[0], ppFftOut[0]);
+
         cout << "\r" << "reading and writing";
 
         // write
-        for (int i = 0; i < iNumFrames; i++)
+        int iMagnitudeLen = pFft->getLength(CFft::kLengthMagnitude);
+        for (int i = 0; i < iMagnitudeLen; i++)
         {
-            for (int c = 0; c < stFileSpec.iNumChannels; c++)
-            {
-                hOutputFile << ppfAudioData[c][i] << "\t";
-            }
-            hOutputFile << endl;
+            hOutputFile << ppfMagnitude[0][i] << "\t";
         }
+        hOutputFile << endl;
+
     }
 
     cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
@@ -138,11 +178,30 @@ int main(int argc, char* argv[])
     delete[] ppfAudioData;
     ppfAudioData = 0;
 
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfBuffer[i];
+    delete[] ppfBuffer;
+    ppfBuffer = 0;
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfBlock[i];
+    delete[] ppfBlock;
+    ppfBlock = 0;
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppFftOut[i];
+    delete[] ppFftOut;
+    ppFftOut = 0;
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfMagnitude[i];
+    delete[] ppfMagnitude;
+    ppfMagnitude = 0;
+
     // all done
     return 0;
 
 }
-
 
 void     showClInfo()
 {
