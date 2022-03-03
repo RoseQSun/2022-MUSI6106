5bcc3a4ea1dc297a45a7d6ff74476652ef030741
RoseQSun <qsun75@gatech.edu>
2022-03-03 01:46:15 -0500

    assignment 2 backup

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -5,22 +5,172 @@
 #include "MUSI6106Config.h"
 
 #include "AudioFileIf.h"
+#include "CombFilterIf.h"
+#include "RingBuffer.h"
+#include "Vibrato.h"
+
+#define M_PIl          3.141592653589793238462643383279502884L /* pi */
+
 using std::cout;
 using std::endl;
 
 // local function declarations
 void    showClInfo ();
 
+typedef struct CombFilterArgs {
+    CCombFilterIf::CombFilterType_t filterType = CCombFilterIf::CombFilterType_t::kCombFIR;
+    float delay = 0.1F;
+    float gain = 0.5F;
+    std::string inputPath = "fake_id.wav";
+    std::string outputPath = "out.wav";
+} CombFilterArgs_t;
+
+typedef struct VibratoArgs {
+    CCombFilterIf::CombFilterType_t filterType = CCombFilterIf::CombFilterType_t::kCombFIR;
+    float vibratoWidth = 0.01F;
+    float vibratoFrequency = 6.6F;
+    std::string inputPath = "sin.wav";
+    std::string outputPath = "out.wav";
+} VibratoArgs_t;
+
+void printUsage() {
+    std::printf("Usage: MUSI6106Exec.exe [option]\n\n");
+    std::printf("Options and arguments:\n");
+    std::printf("-w: The vibrato width in Hz.\n");
+    std::printf("    -w <width>: set the width of vibrato effector in Hz. E.g. for 440 sinusoid & 10 Hz vibrato\n");
+    std::printf("                width, it will oscillate in 430-450Hz.\n");
+    std::printf("-f: The vibrato frequency in Hz.\n");
+    std::printf("    -f <freq>: set the frequency of vibrato effector in Hz. E.g. Frequency 6Hz -> vibrato 6 times/s.\n");
+    std::printf("-i: The path to input wave file.\n");
+    std::printf("    -i <path>: load the wave file of <path>. Notice you need to include filename.\n");
+    std::printf("-o: The path to output wave file.\n");
+    std::printf("    -o <path>: write the filtered signal into <path>. Notice you need to include filename\n");
+    std::printf("-h --help: print this help message.\n");
+    std::printf("Examples:\n");
+    std::printf("- Read 'fake_id.wav' and apply vibrato effector, with vibrato width=10Hz, frequency=6.6Hz,\n");
+    std::printf("  write the filtered signal into 'out.wav':\n");
+    std::printf("  `MUSI6106Exec.exe -w 10 -f 6.6 -i fake_id.wav -o out.wav`\n");
+    std::printf("- Display this help:\n");
+    std::printf("  `MUSI6106Exec.exe -h`\n");
+    std::printf("  or\n");
+    std::printf("  `MUSI6106Exec.exe --help`\n");
+}
+
+/*
+Simple command line argument parser.
+*/
+VibratoArgs_t parseArg(int argc, char** argv) {
+    VibratoArgs_t result;
+    for (int i = 0; i < argc; ++i) {
+        if (argv[i][0] == '-') { /* this is an argument */
+            if (argv[i][1] == 'w') {
+                if (i == argc - 1) throw(std::exception("Incomplete argument of delay!"));
+                result.vibratoWidth = std::stof(argv[++i]);
+            }
+            else if (argv[i][1] == 'f') {
+                if (i == argc - 1) throw(std::exception("Incomplete argument of gain!"));
+                result.vibratoFrequency = std::stof(argv[++i]);
+            }
+            else if (argv[i][1] == 'i') {
+                if (i == argc - 1) throw(std::exception("Incomplete argument of input file path!"));
+                result.inputPath = std::string(argv[++i]);
+            }
+            else if (argv[i][1] == 'o') {
+                if (i == argc - 1) throw(std::exception("Incomplete argument of output file path!"));
+                result.outputPath = std::string(argv[++i]);
+            }
+            else if (argv[i][1] == 'h' || !strcmp(argv[i], "--help")) {
+                printUsage();
+                exit(0);
+            }
+            else {
+                throw(std::exception("unknown argument!"));
+            }
+        }
+    }
+    return result;
+}
+
+void processFile(VibratoArgs_t& args) {
+    static const int kBlockSize = 1024;
+
+    clock_t time = 0;
+
+    /* Initialize input & output audio files */
+    CAudioFileIf *phInputAudioFile = nullptr;
+    CAudioFileIf *phOutputAudioFile = nullptr;
+
+    CAudioFileIf::FileSpec_t stInputFileSpec;
+    CAudioFileIf::FileSpec_t stOutputFileSpec;
+
+    CAudioFileIf::create(phInputAudioFile);
+    CAudioFileIf::create(phOutputAudioFile);
+
+    phInputAudioFile->openFile(args.inputPath, CAudioFileIf::FileIoType_t::kFileRead);
+    phInputAudioFile->getFileSpec(stInputFileSpec);
+    phOutputAudioFile->openFile(args.outputPath, CAudioFileIf::FileIoType_t::kFileWrite, &stInputFileSpec);
+
+    /* Initialize Vibrato Effector */
+    VibratoEffector* vibratoEffector = new VibratoEffector(static_cast<int>(stInputFileSpec.fSampleRateInHz), stInputFileSpec.iNumChannels, args.vibratoFrequency, args.vibratoWidth);
+
+    /* Initialize audio buffer arrays */
+    long long kllBlockSize = static_cast<long long>(kBlockSize);
+
+    float** ppfInputAudioData, **ppfOutputAudioData;
+    ppfInputAudioData = new float* [stInputFileSpec.iNumChannels];
+    ppfOutputAudioData = new float* [stInputFileSpec.iNumChannels];
+    for (int i = 0; i < stInputFileSpec.iNumChannels; ++i) {
+        ppfInputAudioData[i] = new float[kllBlockSize];
+        ppfOutputAudioData[i] = new float[kllBlockSize];
+    }
+
+    /* set lfo shape */
+    float* temp = new float[1 << 12];
+    CSynthesis::generateSine(temp, 1, 1 << 12, 1 << 12);
+    vibratoEffector->setLfoShape(temp);
+    delete[] temp;
+
+    /* Iterate over whole file and apply effect */
+    for (; !phInputAudioFile->isEof(); ) {
+        phInputAudioFile->readData(ppfInputAudioData, kllBlockSize);
+        vibratoEffector->process(ppfInputAudioData, ppfOutputAudioData, kllBlockSize);
+        phOutputAudioFile->writeData(ppfOutputAudioData, kllBlockSize);
+    }
+
+    phOutputAudioFile->closeFile();
+
+    /* Clean up allocated things */
+    for (int i = 0; i < stInputFileSpec.iNumChannels; ++i) {
+        delete [] ppfInputAudioData[i];
+        delete [] ppfOutputAudioData[i];
+    }
+    delete[] ppfInputAudioData;
+    delete[] ppfOutputAudioData;
+
+    delete vibratoEffector;
+    CAudioFileIf::destroy(phInputAudioFile);
+    CAudioFileIf::destroy(phOutputAudioFile);
+}
+
 /////////////////////////////////////////////////////////////////////////////////
 // main function
-int main(int argc, char* argv[])
-{
-    showClInfo();
-
-
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        /* First parse arguments, which is essential for audio file interface & combfilter initialization */
+        try {
+            VibratoArgs_t args = parseArg(argc, argv);
+            processFile(args);
+        }
+        catch (std::exception& e) {
+            std::fprintf(stderr, "Error when trying to parse arguments: %s", e.what());
+        }
+    }
+    else {
+        printUsage();
+        std::printf("[WARN] No argument given.\n");
+    }
     // all done
     return 0;
-
 }
 
 
--- src/inc/RingBuffer.h
+++ src/inc/RingBuffer.h
@@ -59,14 +59,39 @@ public:
         return tValue;
     }
 
+    T get() const {
+        return m_ptBuff[getReadIdx()];
+    }
+
+    T get(int iOffset) const {
+        return m_ptBuff[indexWrapper(getReadIdx() + iOffset)];
+    }
+
     /*! return the value at the current read index
     \param fOffset: read at offset from read index
     \return float the value from the read index
     */
-    T get(float fOffset = 0) const
+    T get(float fOffset, bool print_lowerbound = false) const
     {
-        assert(0); // TODO: implement offset
-        return m_ptBuff[m_iReadIdx];
+        int lowerBound = indexWrapper(static_cast<int>(fOffset));
+        int upperBound = indexWrapper(lowerBound + 1);
+
+        // determine if the upper bound should be enlarged.
+        float fractionPart = abs(fOffset - static_cast<int>(fOffset));
+        
+        if (!(0 <= fractionPart && fractionPart <= 1)) {
+            std::printf("%.8f %.8f\n", fOffset, fractionPart);
+        }
+
+        if (upperBound >= m_iBuffLength) {
+            throw std::exception("The input offset is too big and exceeds the buffer length.");
+        }
+
+        // interpolate between lower & upper bound
+        T fLowerBoundValue = get(lowerBound);
+        T fUpperBoundValue = get(upperBound);
+
+        return (1 - fractionPart) * fLowerBoundValue + fractionPart * fUpperBoundValue;
     }
 
     /*! set buffer content and indices to 0
@@ -128,6 +153,11 @@ public:
     {
         return m_iBuffLength;
     }
+
+    int indexWrapper(int index) const {
+        return (index < 0) ? m_iBuffLength - (-index % m_iBuffLength) : index % m_iBuffLength;
+    }
+
 private:
     CRingBuffer();
     CRingBuffer(const CRingBuffer& that);
@@ -149,3 +179,203 @@ private:
     T* m_ptBuff = 0;            //!< data buffer
 };
 #endif // __RingBuffer_hdr__
+
+
+// #if !defined(__RingBuffer_hdr__)
+// #define __RingBuffer_hdr__
+// 
+// #include <cassert>
+// #include <algorithm>
+// #include <cmath>
+// #include <cstring>
+// 
+// /*! \brief implement a circular buffer of type T
+// */
+// template <class T> 
+// class CRingBuffer
+// {
+// public:
+//     explicit CRingBuffer(int iBufferLengthInSamples) :
+//         m_iBuffLength(iBufferLengthInSamples + 1)
+//     {
+//         assert(iBufferLengthInSamples > 0);
+// 
+//         // allocate and init
+//         // use one empty space to make sure we can separate empty and full simply by
+//         // checking indexies.
+//         m_buffer = new T[iBufferLengthInSamples + 1];
+//         for (int i = 0; i < m_iBuffLength; ++i) {
+//             m_buffer[i] = 0;
+//         }
+// 
+//         // the read and write index, and it represents range [m_iReadIdx, m_iWriteIdx)
+//         // NOTE that it's possible to have (m_iReadIdx > m_iWriteIdx).
+//         m_iReadIdx = 0;
+//         m_iWriteIdx = 0;
+//     }
+// 
+//     virtual ~CRingBuffer()
+//     {
+//         // free memory
+//         delete[] m_buffer;
+//     }
+// 
+//     /*! add a new value of type T to write index and increment write index
+//     \param tNewValue the new value
+//     \return void
+//     */
+//     void putPostInc (T tNewValue)
+//     {
+//         // put the value at the position, no matter if there exist a value
+//         put(tNewValue);
+//         incWriteIdx();
+//         if (getReadIdx() == getWriteIdx()) incReadIdx();
+//     }
+// 
+//     /*! add a new value of type T to write index
+//     \param tNewValue the new value
+//     \return void
+//     */
+//     void put(T tNewValue)
+//     {
+//         m_buffer[getWriteIdx()] = tNewValue;
+//     }
+//     
+//     /*! return the value at the current read index and increment the read pointer
+//     \return float the value from the read index
+//     */
+//     T getPostInc()
+//     {
+//         T value = get();
+// 
+//         // if the container is empty, then we need to also increase write index
+//         if (getReadIdx() == getWriteIdx()) incWriteIdx();
+// 
+//         incReadIdx();
+//         return value;
+//     }
+// 
+//     /*! return the value at the current read index
+//     \return float the value from the read index
+//     */
+//     T get() const
+//     {
+//         return m_buffer[getReadIdx()];
+//     }
+// 
+//     T get(int iOffset) const {
+//         return m_buffer[indexWrapper(getReadIdx() + iOffset)];
+//     }
+// 
+//     /*! return the value at the current read index
+//      * \param fOffset: read at offset from read index
+//      * \return float the value from the read index*/
+//     T get(float fOffset) const {
+// 
+//         int lowerBound = indexWrapper(static_cast<int>(fOffset) + getReadIdx());
+//         int upperBound = indexWrapper(lowerBound + 1);
+// 
+//         // determine if the upper bound should be enlarged.
+//         float integerPart = static_cast<int>(fOffset);
+//         float fractionPart = fOffset - static_cast<float>(integerPart);
+// 
+//         if (upperBound >= m_iBuffLength) {
+//             throw std::exception("The input offset is too big and exceeds the buffer length.");
+//         }
+// 
+//         // interpolate between lower & upper bound
+//         T fLowerBoundValue = get(indexWrapper(lowerBound));
+//         T fUpperBoundValue = get(indexWrapper(upperBound));
+// 
+//         return (1 - fractionPart) * fLowerBoundValue + fractionPart * fUpperBoundValue;
+//     }
+//     
+//     /*! set buffer content and indices to 0
+//     \return void
+//     */
+//     void reset()
+//     {
+//         setReadIdx(0);
+//         setWriteIdx(0);
+//         for (int i = 0; i < m_iBuffLength; ++i) {
+//             m_buffer[i] = 0;
+//         }
+//     }
+// 
+//     /*! return the current index for writing/put
+//     \return int
+//     */
+//     int getWriteIdx() const
+//     {
+//         return m_iWriteIdx;
+//     }
+// 
+//     /*! move the write index to a new position
+//     \param iNewWriteIdx: new position
+//     \return void
+//     */
+//     void setWriteIdx(int iNewWriteIdx)
+//     {
+//         m_iWriteIdx = indexWrapper(iNewWriteIdx);
+//     }
+// 
+//     /*! return the current index for reading/get
+//     \return int
+//     */
+//     int getReadIdx() const
+//     {
+//         return m_iReadIdx;
+//     }
+// 
+//     /*! move the read index to a new position
+//     \param iNewReadIdx: new position
+//     \return void
+//     */
+//     void setReadIdx(int iNewReadIdx)
+//     {
+//         m_iReadIdx = indexWrapper(iNewReadIdx);
+//     }
+// 
+//     /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
+//     \return int
+//     */
+//     int getNumValuesInBuffer() const
+//     {
+//         int iNumValue = getWriteIdx() - getReadIdx();
+//         while (iNumValue < 0) iNumValue += m_iBuffLength;
+//         return iNumValue;
+//     }
+// 
+//     /*! returns the length of the internal buffer
+//     \return int
+//     */
+//     int getLength() const
+//     {
+//         return m_iBuffLength - 1;
+//     }
+// private:
+//     CRingBuffer();
+//     CRingBuffer(const CRingBuffer& that);
+// 
+//     int m_iBuffLength;              //!< length of the internal buffer
+//     T* m_buffer;
+//     int m_iReadIdx, m_iWriteIdx;
+// 
+//     void incReadIdx() {
+//         inc(m_iReadIdx);
+//     }
+// 
+//     void incWriteIdx() {
+//         inc(m_iWriteIdx);
+//     }
+// 
+//     void inc(int& i) {
+//         ++i;
+//         if (i >= m_iBuffLength) i -= m_iBuffLength; // should be faster than modulo
+//     }
+// 
+//     inline int indexWrapper(int index) const {
+//         return (index < 0) ? m_iBuffLength - (-index % m_iBuffLength) : index % m_iBuffLength;
+//     }
+// };
+// #endif // __RingBuffer_hdr__
